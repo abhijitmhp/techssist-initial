0e87887c91250d4310650ffffbf7a388e1ef1f44
jhasketan <jasjena@gmail.com>
2020-11-16 03:30:09 +0530

    FE: removed credentials from repo as it is publicly hosted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCkh_paSb8qPgwrwxzqERqwUY9Fax2I9dI',
-    authDomain: 'techssistdev.firebaseapp.com',
-    databaseURL: 'https://techssistdev.firebaseio.com',
-    projectId: 'techssistdev',
-    storageBucket: 'techssistdev.appspot.com',
-    messagingSenderId: '66837520932',
-    appId: '1:66837520932:web:4410db1eb158440b2aeeb1',
-    measurementId: 'G-8FDW46S8YF',
-  },
-  googleReCaptcha: { siteKey: '6LeMNuMZAAAAANlTi2Dev6fEnTWVuHLAUDrjeeyC' },
+  firebaseConfig: 'copy config object from firebase console',
+  googleReCaptcha: { siteKey: 'copy from google recaptcha site config' },
 };
